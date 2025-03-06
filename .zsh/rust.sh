6eaa1d14569dfc4f2b7d0892ff7bb35411179fc6
gmolveau <2090495+gmolveau@users.noreply.github.com>
2025-03-06 22:04:15 +0100

    xdg edits

--- .zsh/rust.sh
+++ .zsh/rust.sh
@@ -1,3 +1,5 @@
-export CARGO_HOME=${HOME}/.cargo
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
 export CARGO_BIN=${CARGO_HOME}/bin
 export PATH=${PATH}:${CARGO_BIN}
+. "${CARGO_HOME}/env"
