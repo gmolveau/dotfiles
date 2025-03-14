45ffb2272ffcffd587781a849fc90210607bcef3
gmolveau <2090495+gmolveau@users.noreply.github.com>
2025-03-14 21:03:20 +0100

    update rust script

--- .zsh/rust.sh
+++ .zsh/rust.sh
@@ -2,4 +2,4 @@ export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
 export CARGO_HOME="${XDG_DATA_HOME}/cargo"
 export CARGO_BIN=${CARGO_HOME}/bin
 export PATH=${PATH}:${CARGO_BIN}
-. "${CARGO_HOME}/env"
+[ -f "${CARGO_HOME}/env" ] && . "${CARGO_HOME}/env"
