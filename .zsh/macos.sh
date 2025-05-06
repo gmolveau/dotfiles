22fbcba76865b89d40a8d1b2b6519a98b2f0bc32
gmolveau <2090495+gmolveau@users.noreply.github.com>
2025-05-06 21:39:44 +0200

    remove iterm2

--- .zsh/macos.sh
+++ .zsh/macos.sh
@@ -1,7 +1,6 @@
 if [[ "${OSTYPE}" != "darwin"* ]]; then
     return 1
 fi
-source "${HOME}/.zsh/macos/iterm2.sh"
 source "${HOME}/.zsh/macos/aliases.sh"
 source "${HOME}/.zsh/macos/exports.sh"
 source "${HOME}/.zsh/macos/functions.sh"
