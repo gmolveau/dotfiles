8921d93783282c44d2106b479ce0f8eb869bd212
gmolveau <2090495+gmolveau@users.noreply.github.com>
2025-10-21 10:58:24 +0200

    update

--- .zsh/node.sh
+++ .zsh/node.sh
@@ -1,4 +1,5 @@
-mkdir -p "${XDG_CACHE_HOME}"/npm
+mkdir -p "${XDG_CACHE_HOME}/npm"
+mkdir -p "${XDG_CONFIG_HOME}/npm"
 export NODE_BIN=${HOME}/.local/bin
 export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node_repl_history"
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
