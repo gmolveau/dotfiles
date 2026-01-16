f5042c8207564177c56b50bb9cc3da34ff21640e
gmolveau <2090495+gmolveau@users.noreply.github.com>
2026-01-16 22:56:59 +0100

    add linux first install script

---
+++ bin/linux-first-install.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+echo "Install the dotfiles first > https://github.com/gmolveau/dotfiles ;)"
+
+INSTALL="sudo apt-get install -y -q --no-install-recommends"
+
+# update / upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# basic tools
+"${INSTALL}" \
+    tree \
+    git \
+    curl wget \
+    nano vim \
+    terminator
+
+# tmux
+"${INSTALL}" tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# ssh
+ssh-keygen -q -C "" -N "" -t ed25519 -f ~/.ssh/id_ed25519
+
+# 1Password
+"${INSTALL}" gnupg2
+wget https://downloads.1password.com/linux/debian/amd64/stable/1password-latest.deb -O /tmp/1password.deb
+sudo apt install /tmp/1password.deb
+rm /tmp/1password.deb
+
+# Visual Studio Code
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/vscode.deb
+sudo apt install /tmp/vscode.deb
+rm /tmp/vscode.deb
+
+# signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+wget -O-  https://updates.signal.org/static/desktop/apt/signal-desktop.sources | sudo tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
+sudo apt-get update -y && "${INSTALL}" signal-desktop
+
+# docker
+curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+sudo sh /tmp/get-docker.sh
+
+# python
+${INSTALL} python3-dev python3-venv
+
+# nodejs
+curl -sL https://deb.nodesource.com/setup_24.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+"${INSTALL}" nodejs
+
+# databases
+"${INSTALL}" sqlitebrowser postgresql-client
+
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/chrome.deb
+sudo apt install /tmp/chrome.deb
+rm /tmp/chrome.deb
+
+# VLC
+"${INSTALL}" vlc
+
+# UV
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# claude
+curl -fsSL https://claude.ai/install.sh | bash
